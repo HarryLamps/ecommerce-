d53d8b814702e8ffe01b062ec812d36aaed09fff
HarryLamps <115990835+HarryLamps@users.noreply.github.com>
2022-11-03 20:34:35 +0000

    aaa

--- index.php
+++ index.php
@@ -1,7 +1,17 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Document</title>
 </head>
 <body>
-<h1>Hello World<h1>
+	
+<a href="./view/register.php"> Registration Page</a> <br>
+<br>
+<a href="./view/login.php">Login</a>
 </body>
 </html>
+
+	
